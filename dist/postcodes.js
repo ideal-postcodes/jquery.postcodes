74d9745dae2e41304c0c0a133a18e891d75e89cb
Chris Blanchard <cablanchard@gmail.com>
2016-01-25 08:29:54 +0000

    Bump to 3.0.1

--- dist/postcodes.js
+++ dist/postcodes.js
@@ -1,6 +1,6 @@
-/*! Ideal Postcodes jQuery Plugin - v3.0.0 - 2015-06-03
+/*! Ideal Postcodes jQuery Plugin - v3.0.0 - 2016-01-25
 * https://github.com/ideal-postcodes/jquery.postcodes
-2015 Ideal Postcodes; Licensed MIT */
+2016 Ideal Postcodes; Licensed MIT */
 (function($) {
   "use strict";
   // Cache for all new instances of the plugin
